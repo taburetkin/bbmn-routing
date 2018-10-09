3ea3567e86ad022edc99d198e637089ca742155c
dimaver <dima.taburetkin@gmail.com>
2018-10-09 18:08:45 +0300

    exporting pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import Router from './router/index.js';
 import routeErrorHandler from './route-error-handler/index.js';
-import { PagedApp } from './pages/index.js';
+import { PagedApp, PageRouter, Page  } from './pages/index.js';
 export {
 	Router,
 	routeErrorHandler,
-	PagedApp
+	PagedApp, PageRouter, Page
 };
 export default {
 	Router,
 	routeErrorHandler,
-	PagedApp
+	PagedApp, PageRouter, Page
 };
